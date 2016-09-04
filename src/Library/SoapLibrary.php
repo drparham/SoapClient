9d0838a50b6c7e84be186df6ac7c2d7856701eae
Dustin Parham <dr.parham@gmail.com>
2016-09-04 13:33:02 -0600

    modified request method

--- src/Library/SoapLibrary.php
+++ src/Library/SoapLibrary.php
@@ -8,12 +8,15 @@ class SoapLibrary
     private $auth;
     private $user;
     private $pass;
+    private $options;
+    private $client;
 
     public function __construct()
     {
         $config = \Config::get('soap');
         $this->wsdl = $config['wsdl'];
         $this->auth = $config['auth'];
+        $this->options = $config['options'];
         if($this->auth){
             $this->user = $config['user'];
             $this->pass = $config['pass'];
@@ -21,11 +24,21 @@ class SoapLibrary
             $this->user = null;
             $this->pass = null;
         }
+        $this->client = new SoapClient($this->wsdl, $this->options);
     }
 
-    public function request($request)
+    public function request($method, $request)
     {
-        return new SoapClient($this->wsdl, $request);
+        try {
+            $data = $this->client->__soapCall($method,[$request]);
+        }
+        catch(Exception $e) {
+            die($e->getMessage());
+        }
+        $data = simplexml_load_string($data->{$method.'Result'}->any);
+//        $data = json_encode($data);
+//        $data = json_decode($data,TRUE);
+        return $data;
     }
 
 
