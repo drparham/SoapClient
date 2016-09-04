f69d4cfaf0f5b2b2101f86947847172e946bd406
Dustin Parham <dr.parham@gmail.com>
2016-09-04 13:32:49 -0600

    Updated config to include options array

--- config/config.php
+++ config/config.php
@@ -1,8 +1,19 @@
 <?php
 
 return [
-    'wsdl' => 'http://www.webservicex.com/globalweather.asmx?WSDL',
+    'wsdl' => 'http://www.webservicex.net/uszip.asmx?WSDL',
     'auth' => false,
     'user' => '',
     'pass' => '',
+    'options' => [
+            'uri'=>'http://schemas.xmlsoap.org/soap/envelope/',
+            'style'=>SOAP_RPC,
+            'use'=>SOAP_ENCODED,
+            'soap_version'=>SOAP_1_1,
+            'cache_wsdl'=>WSDL_CACHE_NONE,
+            'connection_timeout'=>15,
+            'trace'=>true,
+            'encoding'=>'UTF-8',
+            'exceptions'=>true,
+    ],
 ];
